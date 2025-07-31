cfd68ea8faf12e2566734bd5894244cc0a8af541
Tofig Farajullayev <tofiq.ferecullayev@gmail.com>
2025-08-01 00:12:36 +0400

    test

--- WeatherMcpServer/Program.cs
+++ WeatherMcpServer/Program.cs
@@ -16,9 +16,9 @@ builder.Services.AddHttpClient<ClaudeAiAdapter>();
 builder.Services.AddScoped<IAiAdapter, ClaudeAiAdapter>();
 
 builder.Services
-    .AddMcpServer()
-    .WithStdioServerTransport()
-    .WithTools<WeatherTools>()
-    .WithTools<RandomNumberTools>(); 
+   .AddMcpServer()
+   .WithStdioServerTransport()
+   .WithTools<WeatherTools>()
+   .WithTools<RandomNumberTools>();
 
 await builder.Build().RunAsync();
